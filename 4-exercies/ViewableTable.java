3a173a1dd7d232a236f9f647bc934c4fb66a3a54
StasIhnatishin <137786669+StasIhnatishin@users.noreply.github.com>
2025-03-11 11:36:20 +0200

    Update ViewableTable.java

--- 4-exercies/ViewableTable.java
+++ 4-exercies/ViewableTable.java
@@ -1,23 +1,8 @@
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Клас, що представляє фабрику для створення об'єктів для відображення таблиці.
  */
-public class ViewableTable {
-
-    private List<Item2d> results = new ArrayList<>();
-
-    public void addResult(Item2d result) {
-        results.add(result);
-    }
-
-    public List<Item2d> getResults() {
-        return results;
-    }
-
-    public void displayResults() {
-        ViewTable viewTable = new ViewTable();
-        viewTable.displayTableWithHeader(results);
-    }
+interface ViewableTable {
+    void displayResults(List<Item2d> results);
 }
